420c1710f31297257dcbcaa6aadc563552d14b6b
Kirsiasc <kirsiasc@gmail.com>
2025-07-07 15:02:52 +0900

    main.lua

--- main.lua
+++ main.lua
@@ -1,94 +1,123 @@
-local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "STREE HUB",
-    Icon = "earth", -- Use Lucide icon name as a string, or use image ID number
-    LoadingTitle = "STREE LOADING",
-    LoadingSubtitle = "Made by: Kirsia",
-    Theme = "DarkBlue",
+   Name = "STREE HUB | PRRI SCRIPT",
+   Icon = 101447877507131,
+   LoadingTitle = "SCRIPT Loading...",
+   LoadingSubtitle = "made by community: STREE HUB",
+   ShowText = "STREE",
+   Theme = "Green",
 
-    ToggleUIKeybind = "K",
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
 
-    DisableRayfieldPrompts = false,
-    DisableBuildWarnings = false,
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "STREE HUB",
+      FileName = "STREE Hub"
+   },
 
-    ConfigurationSaving = {
-        Enabled = true,
-        FolderName = "STREE",
-        FileName = "STREE HUB"
-    },
+   Discord = {
+      Enabled = true,
+      Invite = "jdmX43t5mY",
+      RememberJoins = true
+   },
 
-    Discord = {
-        Enabled = true,
-        Invite = "MFzWcQNA",
-        RememberJoins = true
-    },
-
-    KeySystem = true,
-    KeySettings = {
-        Title = "STREE HUB",
-        Subtitle = "STREE Key System",
-        Note = "Get the key from our Discord: https://discord.gg/tTqtKwnGZz",
-        FileName = "Key",
-        SaveKey = true,
-        GrabKeyFromSite = false,
-        Key = {"Free Key", "Free login"}
-    }
+   KeySystem = true,
+   KeySettings = {
+      Title = "STREE HUB",
+      Subtitle = "STREE HUB Key System",
+      Note = "Join Discord dan WhatsApp untuk dapatkan key",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"Free Key", "Free Login","PPRI TETAP JAYA"}
+   }
 })
 
-local UniversalTab = Window:CreateTab("Universal Scripts", "globe-lock")
-UniversalTab:CreateSection("Cheats")
+-- HOME TAB
+local HomeTab = Window:CreateTab("Home", "house")
+local Section1 = HomeTab:CreateSection("Channel")
 
-UniversalTab:CreateButton({
-    Name = "Farm Bond",
-    Callback = function()
-        loadstring(game:HttpGet("https://pastefy.app/qE3kiwX4/raw"))()
-    end,
+HomeTab:CreateButton({
+   Name = "Discord",
+   Callback = function()
+       setclipboard("https://discord.gg/tTqtKwnGZz")
+       Rayfield:Notify({ Title = "Copied", Content = "Discord link copied!", Duration = 2 })
+   end,
 })
 
-UniversalTab:CreateButton({
-    Name = "Auto Win",
-    Callback = function()
-        loadstring(game:HttpGet("https://pastefy.app/sBQRgxba/raw"))()
-    end,
+HomeTab:CreateButton({
+   Name = "Channel WhatsApp",
+   Callback = function()
+       setclipboard("https://whatsapp.com/channel/0029VbAwRihKAwEtwyowt62N")
+       Rayfield:Notify({ Title = "Copied", Content = "WhatsApp link copied!", Duration = 2 })
+   end,
 })
 
-UniversalTab:CreateButton({
-    Name = "WalkSpeed",
-    Callback = function()
-        loadstring(game:HttpGet("https://pastefy.app/QvT5KWcH/raw"))()
-    end,
+-- GAME SCRIPT TAB
+local ScriptTab = Window:CreateTab("Game Script", "earth")
+local Section2 = ScriptTab:CreateSection("Cheating")
+
+ScriptTab:CreateToggle({
+   Name = "Fly",
+   CurrentValue = false,
+   Flag = "FlyToggle",
+   Callback = function(Fly)
+       loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Fly.txt"))()
+   end,
 })
 
-UniversalTab:CreateButton({
-    Name = "Jump Power",
-    Callback = function()
-        loadstring(game:HttpGet("https://pastefy.app/mWz3tpuy/raw"))()
-    end,
+ScriptTab:CreateSlider({
+   Name = "WalkSpeed",
+   Range = {15, 150},
+   Increment = 10,
+   Suffix = "WS",
+   CurrentValue = 10,
+   Flag = "WalkSpeedSlider",
+   Callback = function(V)
+       game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = V
+   end,
 })
 
-local GameTab = Window:CreateTab("Game Scripts", "gamepad-2")
-GameTab:CreateSection("Popular Scripts")
-
-GameTab:CreateButton({
-    Name = "Thand Hub",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/thiennrb7/Script/main/autobond"))()
-    end,
+ScriptTab:CreateSlider({
+   Name = "JumpPower",
+   Range = {15, 150},
+   Increment = 10,
+   Suffix = "JP",
+   CurrentValue = 10,
+   Flag = "JumpPowerSlider",
+   Callback = function(V)
+       game.Players.LocalPlayer.Character.Humanoid.JumpPower = V
+   end,
 })
 
-GameTab:CreateButton({
-    Name = "Null Fire",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/JonasThePogi/DeadRails/main/newloadstring"))()
-    end,
+-- ESP TAB
+local ESPTab = Window:CreateTab("ESP", "eye")
+ESPTab:CreateButton({
+   Name = "ESP Script",
+   Callback = function()
+       loadstring(game:HttpGet("https://pastefy.app/AoZzV4ct/raw"))()
+   end,
 })
 
+-- AIMBOT TAB
+local AimbotTab = Window:CreateTab("AimBot", "crosshair")
+AimbotTab:CreateButton({
+   Name = "AimBot Script",
+   Callback = function()
+       loadstring(game:HttpGet("https://pastefy.app/ZZZPEYZQ/raw"))()
+   end,
+})
+
+-- NOTIFY
 Rayfield:Notify({
-    Title = "STREE LOADING",
-    Content = "All scripts successfully loaded!",
-    Duration = 3,
-    Image = "Check",
+   Title = "STREE HUB Loaded",
+   Content = "Harap tunggu 3 detik karena sedang Loading...",
+   Duration = 3,
+   Image = "check",
 })
 
+-- LOAD CONFIG
 Rayfield:LoadConfiguration()
+

49d0be8b276a790950e0033631aef9f6805d2c01
Kirsiasc <kirsiasc@gmail.com>
2025-07-10 13:22:28 +0900

    ESP.lua

---
+++ ESP.lua
@@ -0,0 +1,6 @@
+if SETTINGS.ESP and not hasESP then
+    local esp = Instance.new("Highlight", p.Character)
+    esp.Name = "PelimenESP"
+    esp.FillColor = Color3.fromRGB(255, 0, 0)
+    esp.OutlineColor = Color3.fromRGB(255, 255, 255)
+end

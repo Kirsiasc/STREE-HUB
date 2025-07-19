d94251eacebe4ea9b5028589afcbdd3488be9774
Kirsiasc <kirsiasc@gmail.com>
2025-07-19 15:22:05 +0900

    InfiniteJump.lua

--- InfiniteJump.lua
+++ InfiniteJump.lua
@@ -1,8 +1,17 @@
-uis.JumpRequest:Connect(function()
-    if UNLOCKED and SETTINGS.InfiniteJump then
-        local hum = plr.Character and plr.Character:FindFirstChildWhichIsA("Humanoid")
-        if hum then
-            hum:ChangeState(Enum.HumanoidStateType.Jumping)
+-- Infinite Jump by Kirsiasc
+local UserInputService = game:GetService("UserInputService")
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
+local UNLOCKED = true -- Ganti ke false kalau kamu pakai sistem key
+local InfiniteJumpEnabled = true -- Atur ke false kalau mau nonaktifkan manual
+
+UserInputService.JumpRequest:Connect(function()
+    if UNLOCKED and InfiniteJumpEnabled then
+        local character = player.Character
+        local humanoid = character and character:FindFirstChildWhichIsA("Humanoid")
+        if humanoid then
+            humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
         end
     end
 end)

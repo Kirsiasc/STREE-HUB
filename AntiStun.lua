32116315ac2c4caf2d4054fbe0060c4127148931
Kirsiasc <kirsiasc@gmail.com>
2025-07-19 16:03:51 +0900

    AntiStun.lua

--- AntiStun.lua
+++ AntiStun.lua
@@ -1,2 +1,9 @@
-if char:FindFirstChild("Ragdoll") then char:FindFirstChild("Ragdoll"):Destroy() end
-if char:FindFirstChild("Stunned") then char:FindFirstChild("Stunned"):Destroy() end
+run.RenderStepped:Connect(function()
+    if UNLOCKED and SETTINGS.AntiStun and plr.Character then
+        local char = plr.Character
+        if char:FindFirstChild("Ragdoll") then char:FindFirstChild("Ragdoll"):Destroy() end
+        if char:FindFirstChild("Stunned") then char:FindFirstChild("Stunned"):Destroy() end
+        local hum = char:FindFirstChildOfClass("Humanoid")
+        if hum then hum.PlatformStand = false end
+    end
+end)
